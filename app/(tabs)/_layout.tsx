aebd1a56199b247ba2137e24e57d32b9d76fdb0f
Theo Fortune <adjissi070@gmail.com>
2025-10-25 01:14:23 +0100

    navigation tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,102 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Tabs } from "expo-router";
+import React from "react";
+import { Image, ImageBackground, Text, View } from "react-native";
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+const TabIcon = ({ focused, icon, title }: any) => {
+  if (focused) {
+    return (
+      <ImageBackground
+        source={images.highlight}
+        className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
+      >
+        <Image source={icon} tintColor="#151312" className="size-5" />
+        <Text className="text-secondary text-base font-semibold ml-2">
+          {title}
+        </Text>
+      </ImageBackground>
+    );
+  }
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
+    <View className="size-full justify-center items-center mt-4 rounded-full">
+      <Image source={icon} tintColor={"#A8b5db"} className="size-5"/>
+    </View>
+  )
+};
+
+const _Layout = () => {
+  return (
+    <Tabs screenOptions={{
+      tabBarShowLabel: false,
+      tabBarItemStyle: {
+        width: "100%",
+        height: "100%",
+        justifyContent: "center",
+        alignItems: "center",
+      },
+      tabBarStyle: {
+        backgroundColor: "#0f0d23",
+        borderRadius: 50,
+        marginHorizontal: 20,
+        marginBottom: 36,
+        height: 52,
+        position: "absolute",
+        overflow: "hidden",
+        borderWidth: 1,
+        borderColor: "#0f0d23"
+      }
+    }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <>
+              <TabIcon focused={focused} icon={icons.home} title="Home" />
+            </>
+          ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="search"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "Search",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <>
+              <TabIcon focused={focused} icon={icons.search} title="Search" />
+            </>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="saved"
+        options={{
+          title: "Saved",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <>
+              <TabIcon focused={focused} icon={icons.save} title="Saved" />
+            </>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <>
+              <TabIcon focused={focused} icon={icons.person} title="Profile" />
+            </>
+          ),
         }}
       />
     </Tabs>
   );
-}
+};
+
+export default _Layout;
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
+  // Include the `app/` directory (used by expo-router) and common source folders.
+  content: [
+    "./App.tsx",
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary: "#151312",
+        light: {
+          100: "#dcc6ff",
+          200: "#a8b5db",
+          300: "#9ca4ab",
+        },
+        dark:{
+          100: "#221f3d",
+          200: "#0f0d23"
+          
+        },
+        accent: "#ab8bff",
+      },
+    },
+  },
+  plugins: [],
+};
